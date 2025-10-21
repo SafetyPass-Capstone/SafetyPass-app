857f46eeb4f8043950427efda166832c8264ec07
Seoneui <ellieella2031@gmail.com>
2025-10-21 15:44:03 +0900

    feat: main에서 provider 구독

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,17 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 import 'package:safetypass_app/route/routes.dart';
 
 void main() async {
-  runApp(const MyApp());
+  runApp(
+    MultiProvider(
+      providers: [
+        /// 앱 전역으로 공유할 상태 추가
+        //ChangeNotifierProvider(create: (_) => ),
+      ],
+      child: const MyApp(),
+    ),
+  );
 }
 
 class MyApp extends StatelessWidget {
