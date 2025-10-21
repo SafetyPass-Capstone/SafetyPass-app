1d8e8c495249cbefd4357909570576e86a5dbc55
Seoneui <ellieella2031@gmail.com>
2025-10-21 16:07:49 +0900

    design: 컬러 추가

---
+++ lib/constants/colors.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+final class SafetPassColor {
+  // Brand Color
+  static const blue = Color(0xFF000000);
+  static const green = Color(0xFF51EC8B);
+
+  static const systemGray05 = Color(0xFF000000);
+  static const systemGray04 = Color(0xFF626262);
+  static const systemGray03 = Color(0xFFAEAEB2);
+  static const systemGray02 = Color(0xFFEBEBF0);
+  static const systemGray01 = Color(0xFFF4F4F4);
+
+  static const white = Color(0xFFFFFFFF);
+  static const bk5 = Color(0xFF000000);
+
+  static const systemRed = Color(0xFFFF3B30);
+}
