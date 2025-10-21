3fdcaf8eac00ec3c0583e53c01b3db5e26825047
Seoneui <ellieella2031@gmail.com>
2025-10-21 16:00:19 +0900

    rebase main

---
+++ lib/widgets/atoms/texts/styles.dart
@@ -0,0 +1,53 @@
+import 'package:flutter/material.dart';
+
+final class SafetyPassTextStyle {
+  static const TextStyle appTittle = TextStyle(
+    fontFamily: "AlkatraSemibold",
+    fontSize: 50,
+  );
+
+  static const TextStyle titleSB24 = TextStyle(
+    fontFamily: "PretendardSemiBold",
+    fontSize: 24,
+  );
+
+  static const TextStyle bodyEB25 = TextStyle(
+    fontFamily: "PretendardExtraBold",
+    fontSize: 25,
+  );
+
+  static const TextStyle bodySB20 = TextStyle(
+    fontFamily: "PretendardSemiBold",
+    fontSize: 20,
+  );
+
+  static const TextStyle bodyEB20 = TextStyle(
+    fontFamily: "PretendardExtraBold",
+    fontSize: 20,
+  );
+
+  static const TextStyle bodyEB17 = TextStyle(
+    fontFamily: "PretendardExtraBold",
+    fontSize: 17,
+  );
+
+  static const TextStyle bodySB17 = TextStyle(
+    fontFamily: "PretendardSemiBold",
+    fontSize: 17,
+  );
+
+  static const TextStyle bodyR15 = TextStyle(
+    fontFamily: "PretendardRegular",
+    fontSize: 15,
+  );
+
+  static const TextStyle bodyR12 = TextStyle(
+    fontFamily: "PretendardRegular",
+    fontSize: 12,
+  );
+
+  static const TextStyle bodyEB10 = TextStyle(
+    fontFamily: "PretendardExtraBold",
+    fontSize: 10,
+  );
+}
